3150fa18554e5d392f9841ec077c93a550328279
Michael Akao <michaelakao@gmail.com>
2023-03-20 10:31:14 -0300

    testando

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ console.log("Rodando o sistema de gerenciamento de Jogos")
 console.log("aplicação está se conectando ao banco de dados")
 // Adicionando frase para igualar com aula
 console.log("igualar frases com aula")
+// testando
