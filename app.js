47183205fbfaffdc83a935635ec276346539d308
Michael Akao <michaelakao@gmail.com>
2023-03-20 09:01:01 -0300

    voltando um commit

--- app.js
+++ app.js
@@ -2,4 +2,3 @@ console.log("Rodando o sistema de gerenciamento de Jogos")
 console.log("aplicação está se conectando ao banco de dados")
 // Adicionando frase para igualar com aula
 console.log("igualar frases com aula")
-console.log("mais uma mensagem para igualar com aula")
